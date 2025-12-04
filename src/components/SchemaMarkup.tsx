8ae4ed5576051d0a663bd6d853512c43f29bd245
Om Dahale <omdahale01@gmail.com>
2025-12-04 16:51:27 +0530

    feat: Add robots.txt and sitemap.xml, and update SEO meta tags in index.html.

---
+++ src/components/SchemaMarkup.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+interface SchemaMarkupProps {
+    schema: Record<string, any>;
+}
+
+const SchemaMarkup: React.FC<SchemaMarkupProps> = ({ schema }) => {
+    return (
+        <Helmet>
+            <script type="application/ld+json">
+                {JSON.stringify(schema)}
+            </script>
+        </Helmet>
+    );
+};
+
+export default SchemaMarkup;
